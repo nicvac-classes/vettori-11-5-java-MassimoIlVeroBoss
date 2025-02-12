c35363df6bd199116b6072e047675f768bed392f
MassimoIlVeroBoss <claudio.mercoledisanto@marconibari.edu.it>
2025-02-12 09:17:43 +0000

    consegna

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,56 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
 
-// Classe principale, con metodo main
 class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+    static int ELiminaElemento(int[] V, int N, int ie) {
+        int N2 = N - 1;
+        for (int i = ie; i < N2; i++) {
+            V[i] = V[i + 1];
+        }
+        return N2;
+    }
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N;
+        int coppie = 0;
+        int i , j;
+        boolean trovato;
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        do {
+            System.out.print("Inserire Dimensione vettore: ");
+            N = in.nextInt();
+            if (N <= 0) {
+                System.out.println("Inserire un numero maggiore di 0!");
+            }
+        } while (N <= 0);
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        int[] V = new int[N];
+
+        for(i = 0; i < N; i++){
+            V[i] = 4 + (int)(Math.random() * 7);
+            System.out.println(i + "°numero: " + V[i]);
+        }
+
+        i = 0;
+        while(i < N - 1){
+            j = i + 1;
+            trovato = false;
+            while(j < N && !trovato){
+                if(V[i] == V[j]){
+                    trovato = true;
+                    N = ELiminaElemento(V, N, j);
+                    N = ELiminaElemento(V, N, i);
+                    coppie ++;
+                    i = -1;
+                }else{
+                    j++;
+                }
+            }
+            i++;
+        }
+
+        coppie = coppie / 2;
+
+        System.out.println("Possono essere realizzate massimo " + coppie + " cornici");
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
